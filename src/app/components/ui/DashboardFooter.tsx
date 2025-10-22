6a7f013995c96542414c875d18dc284c142b4ac7
Giancarlo Silva <giancarlozero@gmail.com>
2025-10-21 23:51:09 -0300

    CSS limpo e atualizado em alguns componentes.

--- src/app/components/ui/DashboardFooter.tsx
+++ src/app/components/ui/DashboardFooter.tsx
@@ -4,11 +4,9 @@ import styles from "@/app/components/ui/dashboardfooter.module.css"
 
 export default function DashboardFooter(){
   return(
-    <footer className={["container-fluid position-fixed bottom-0 z-2", styles.rodape].join(" ")}>
-      <div className="row p-3">
-        <div className="col d-flex flex-row align-items-center justify-content-center">
-          <p>Rodapé</p>
-        </div>
+    <footer className={["w-screen bottom-0 flex flex-row justify-center fixed", styles.rodape].join(" ")}>
+      <div>
+        <p>Rodapé</p>
       </div>
     </footer>
   );
--- src/app/components/ui/DashboardHeader.tsx
+++ src/app/components/ui/DashboardHeader.tsx
@@ -2,22 +2,21 @@
 import styles from '@/app/components/ui/dashboardheader.module.css';
 import DashboardLogo from './DashboardLogo';
 import DashboardNav from "./DashboardNav";
-// import DashboardNavMobile from './DashboardNavMobile';
 import SessionNav from "./SessionNav";
 
 export default function DashboardHeader(){
   return(
-    <header className={["container-fluid fixed-top", styles.cabecalho].join(" ")}>
-      <div className="row">
-        <div className={["col-2 d-flex flex-row align-items-center justify-content-start p-2", styles.logo].join(" ")}>
-          <DashboardLogo />
-        </div>
-        <div className={["col d-flex flex-row align-items-center justify-content-center p-2", styles.topnav].join(" ")}>
+    <header className={["w-screen top-0 flex flex-row items-center justify-center fixed", styles.cabecalho].join(" ")}>
+      <div className={["p-2", styles.logo].join(" ")}>
+        <DashboardLogo />
+      </div>
+      <div className={["grow p-2", styles.topnav].join(" ")}>
+        <nav className={["flex flex-row gap-1.5 items-center justify-center ", styles.navegacao].join(" ")}>
           <DashboardNav />
-        </div>
-        <div className={["col-2 d-flex flex-row align-items-center justify-content-end p-2 gap-2", styles.userprofile].join(" ")}>
-          <SessionNav />
-        </div>
+        </nav>
+      </div>
+      <div className={["p-2 gap-2", styles.userprofile].join(" ")}>
+        <SessionNav />
       </div>
     </header>
   );
