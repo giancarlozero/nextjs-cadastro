f73a07befb6a658b5c1b5d8e1fdd8a30b55a981c
Giancarlo Silva <giancarlozero@gmail.com>
2025-11-03 20:27:30 -0300

    Pequena alteração.

--- src/app/components/servicos/criarservicoform.tsx
+++ src/app/components/servicos/criarservicoform.tsx
@@ -29,8 +29,6 @@ export default function CriarServicoForm() {
     <div className={["secao-dados flex flex-col md:flex-row m-2 p-2 mb-3 gap-2 border rounded", styles.boxConteudo].join(" ")}>
       <div className="info w-[100%] md:w-[40%]">
         <p>Preencha o formulário e crie um novo serviço neste sistema com os dados desejados. Defina aqui o título/nome do serviço, o preço a ser cobrado por ele e dê uma breve descrição do serviço para facilitar o entendimento.</p>
-
-        <p><strong>Todos os campos são obrigatórios.</strong></p>
       </div>
       <div className="formulario w-[100%] md:w-[60%]">
         <form onSubmit={handleSubmit}>
@@ -48,14 +46,20 @@ export default function CriarServicoForm() {
 
           <div className="flex flex-col md:flex-row gap-3">
             <div className="w-[100%]">
-              <FieldLabel htmlFor="descricao">Descrição</FieldLabel>
-              <Textarea name="descricao" className="w-full bg-white rounded p-2 mb-1" id="descricao" rows="5" required />
+              <FieldLabel htmlFor="descricao">Descrição - OPCIONAL</FieldLabel>
+              <Textarea name="descricao" className="w-full bg-white rounded p-2 mb-1" id="descricao" rows={5} />
             </div>
           </div>
 
           <div className="flex flex-row gap-2 align-end justify-end-safe">
-            <Link className="bg-gray-500 hover:bg-gray-600 text-white px-2 py-1 rounded" href="/painel/servicos">Cancelar</Link>
-            <button className="bg-green-700 hover:bg-green-800 text-white px-2 py-1 rounded" type="submit">Criar serviço</button>
+            <Link
+              className="bg-gray-500 hover:bg-gray-600 text-white px-2 py-1 rounded"
+              href="/painel/servicos"
+            >Cancelar</Link>
+            <button
+              className="bg-green-700 hover:bg-green-800 text-white px-2 py-1 rounded"
+              type="submit"
+            >Criar serviço</button>
           </div>
         </form>
       </div>
